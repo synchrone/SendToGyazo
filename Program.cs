25deaa9db887ab117334f3f38e2f2906e534a817
Alexander Bogdanov <syn@li.ru>
2011-08-16 20:40:21 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using WebHelper;
+using System.Security;
+using System.Security.Cryptography;
+using System.Configuration;
+
+
+namespace SendToGyazo
+{
+    static class Program
+    {
+        static NotifyIcon i = new NotifyIcon()
+        {
+            Text = "SendToGyazo",
+            Icon = SendToGyazo.Properties.Resources.icon,
+            Visible = true
+        };
+        static string postURL = "http://gyazo.com/upload.cgi";
+        static string userAgent = "Gyazowin/1.0";
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Stream imgStream;
+            string msg = "";
+            if (args.Length != 1 && Clipboard.ContainsImage())
+            {
+                imgStream = new MemoryStream();
+                Clipboard.GetImage().Save(imgStream, System.Drawing.Imaging.ImageFormat.Png);
+                imgStream.Seek(0, SeekOrigin.Begin);
+                msg = "Got data from clipboard";
+            }
+            else if (args.Length == 1)
+            {
+                // Read file data
+                imgStream = new FileStream(args[0], FileMode.Open, FileAccess.Read);
+                
+                msg = "Got data from cmdline arguments";
+            }
+            else {
+                return;
+            }
+
+            byte[] data = new byte[imgStream.Length];
+            imgStream.Read(data, 0, data.Length);
+            imgStream.Close();
+            i.ShowBalloonTip(5000, "Uploading image", msg, ToolTipIcon.Info);
+            
+
+            // Generate post objects
+            Dictionary<string, object> postParameters = new Dictionary<string, object>();
+            postParameters.Add("id", new SHA1CryptoServiceProvider().ComputeHash(data));
+            postParameters.Add("imagedata", new WebHelpers.FileItem("gyazo.com", data));
+
+            System.Net.ServicePointManager.Expect100Continue = false; //okay .net that's silly
+            HttpWebResponse webResponse = WebHelpers.MultipartFormDataPost(postURL, userAgent, postParameters,
+                "----BOUNDARYBOUNDARY----"
+            );
+            // Process response
+            StreamReader responseReader = new StreamReader(webResponse.GetResponseStream());
+            string fullResponse = responseReader.ReadToEnd();
+            responseReader.Close();
+            webResponse.Close();
+
+
+            EventHandler hndlr = delegate
+            {
+                Clipboard.SetText(fullResponse);
+                Application.Exit();
+            };
+
+            i.DoubleClick += hndlr;
+            i.BalloonTipClicked += hndlr;
+
+            i.ShowBalloonTip(5000, "Image sent to gyazo.com", "Click balloon or Doubleclick icon to copy it's url", ToolTipIcon.Info);
+            Application.Run();
+        }
+    }
+}
---
+++ Uploader.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Xml;
+
+namespace libGyazo
+{
+    public class Uploader
+    {
+        public Uploader(){
+            
+        }
+        public string Upload(string filename) {
+            NameValueCollection pars = new NameValueCollection();
+            pars.Add("id", "00000");
+
+            XmlDocument responseDoc = new XmlDocument();
+            string baseUrl = "http://gyazo.com";
+            try
+            {
+                string response = UploadFileEx(filename, baseUrl + "/upload.cgi", "imagedata",
+                    GetMimeType(filename), pars, new CookieContainer());
+                return baseUrl + "/files"+response;
+            }catch (Exception e) {
+                return e.Message + "\n\n" + e.StackTrace;
+            }
+        }
+        private string GetMimeType(string fileName)
+        {
+            string mimeType = "application/unknown";
+            string ext = System.IO.Path.GetExtension(fileName).ToLower();
+            Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(ext);
+            if (regKey != null && regKey.GetValue("Content Type") != null)
+                mimeType = regKey.GetValue("Content Type").ToString();
+            return mimeType;
+        }
+
+        public string UploadFileEx(string uploadfile, string url,string fileFormName, 
+            string contenttype, NameValueCollection querystring, CookieContainer cookies
+        ){
+            if ((fileFormName == null) ||
+                (fileFormName.Length == 0))
+            {
+                fileFormName = "file";
+            }
+
+            if ((contenttype == null) ||
+                (contenttype.Length == 0))
+            {
+                contenttype = "application/octet-stream";
+            }
+
+
+            string postdata;
+            postdata = "?";
+            if (querystring != null)
+            {
+                foreach (string key in querystring.Keys)
+                {
+                    postdata += key + "=" + querystring.Get(key) + "&";
+                }
+            }
+            Uri uri = new Uri(url + postdata);
+
+
+            string boundary = "----------" + DateTime.Now.Ticks.ToString("x");
+            HttpWebRequest webrequest = (HttpWebRequest)WebRequest.Create(uri);
+            webrequest.CookieContainer = cookies;
+            webrequest.ContentType = "multipart/form-data; boundary=" + boundary;
+            webrequest.Method = "POST";
+
+
+            // Build up the post message header
+            StringBuilder sb = new StringBuilder();
+            sb.Append("--");
+            sb.Append(boundary);
+            sb.Append("\r\n");
+            sb.Append("Content-Disposition: form-data; name=\"");
+            sb.Append(fileFormName);
+            sb.Append("\"; filename=\"");
+            sb.Append(Path.GetFileName(uploadfile));
+            sb.Append("\"");
+            sb.Append("\r\n");
+            sb.Append("Content-Type: ");
+            sb.Append(contenttype);
+            sb.Append("\r\n");
+            sb.Append("\r\n");
+
+            string postHeader = sb.ToString();
+            byte[] postHeaderBytes = Encoding.UTF8.GetBytes(postHeader);
+
+            // Build the trailing boundary string as a byte array
+            // ensuring the boundary appears on a line by itself
+            byte[] boundaryBytes = Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
+
+            FileStream fileStream = new FileStream(uploadfile, FileMode.Open, FileAccess.Read);
+            long length = postHeaderBytes.Length + fileStream.Length + boundaryBytes.Length;
+            webrequest.ContentLength = length;
+            webrequest.AllowAutoRedirect = false;
+            Stream requestStream = webrequest.GetRequestStream();
+
+            // Write out our post header
+            requestStream.Write(postHeaderBytes, 0, postHeaderBytes.Length);
+
+            // Write out the file contents
+            byte[] buffer = new Byte[checked((uint)Math.Min(4096, (int)fileStream.Length))];
+            int bytesRead = 0;
+            while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
+                requestStream.Write(buffer, 0, bytesRead);
+
+            // Write out the trailing boundary
+            requestStream.Write(boundaryBytes, 0, boundaryBytes.Length);
+            WebResponse responce = webrequest.GetResponse();
+            string[] locHeaders = responce.Headers.GetValues("Location");
+
+            if (locHeaders!=null && locHeaders.Length == 1)
+            {
+                return locHeaders[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
